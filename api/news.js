7cf63689947b4c6471d79bc0bfa1a9564d7054c8
poyenchen90-ui <poyen.chen90@gmail.com>
2026-03-12 11:49:52 +0100

    Update news.js

--- api/news.js
+++ api/news.js
@@ -1,24 +1,33 @@
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
 
-  const url = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/News?maxRecords=4&sort[0][field]=date&sort[0][direction]=desc`;
+  try {
+    const url = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/zc_news`
+      + `?filterByFormula={active}=1`
+      + `&sort[0][field]=date&sort[0][direction]=desc`
+      + `&maxRecords=4`;
 
-  const r = await fetch(url, {
-    headers: { Authorization: `Bearer ${process.env.AIRTABLE_TOKEN}` }
-  });
+    const r = await fetch(url, {
+      headers: { Authorization: `Bearer ${process.env.AIRTABLE_TOKEN}` }
+    });
 
-  const data = await r.json();
+    const data = await r.json();
 
-  const articles = data.records.map(rec => ({
-    id: rec.id,
-    title: rec.fields.title,
-    excerpt: rec.fields.excerpt,
-    category: rec.fields.category,
-    date: rec.fields.date,
-    author: rec.fields.author,
-    image: rec.fields.image?.[0]?.url || rec.fields.image_url,
-    link: rec.fields.link || '#'
-  }));
+    const articles = data.records.map(rec => ({
+      id: rec.id,
+      title: rec.fields.title || '',
+      excerpt: rec.fields.excerpt || '',
+      category: rec.fields.category || '',
+      date: rec.fields.date || '',
+      author: rec.fields.authour || '',
+      image: rec.fields.image?.[0]?.url || '',
+      link: rec.fields.link || '#'
+    }));
 
-  res.status(200).json(articles);
+    res.status(200).json(articles);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
